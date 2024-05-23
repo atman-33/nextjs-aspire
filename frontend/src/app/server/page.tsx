a2856d7c994e1607aef27aa7ff05a49db5ee924a
atman <gpbjk0304@gmail.com>
2024-05-24 00:47:36 +0900

    feat: webapiを叩く - ServerComponent -

--- frontend/src/app/server/page.tsx
+++ frontend/src/app/server/page.tsx
@@ -1,11 +1,22 @@
+const getData = async () => {
+  const apiServer = process.env['services__webapi__https__0'] ?? process.env['services__webapi__http__0']
+  const weatherData: Response = await fetch(`${apiServer}/api/weatherforecast`, {cache: 'no-cache'})
 
-const ServerPage = () => {
-  console.log('running in server')
+  if(!weatherData.ok) {
+    throw new Error('Failed to fetch data.')
+  }
+
+  const data = await weatherData.json()
+  return data;
+}
+
+
+const ServerPage = async () => {
+  // console.log('running in server')
+  const data = await getData();
 
   return (
-    <main>
-      <div>Server Page</div>
-    </main>
+    <main>{JSON.stringify(data)}</main>
   )
 }
 
