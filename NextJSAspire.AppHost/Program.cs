956945fc56035e8615bb8c9643056790f90cf5b9
atman <gpbjk0304@gmail.com>
2024-05-23 23:53:05 +0900

    feat(apphost): Next.js を .net aspire 管理下に追加

--- NextJSAspire.AppHost/Program.cs
+++ NextJSAspire.AppHost/Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.WebApi>("webapi");
+//builder.AddProject<Projects.WebApi>("webapi");
+
+var api = builder.AddProject<Projects.WebApi>("webapi");
+
+var frontend = builder.AddNpmApp(name: "frontend", workingDirectory: "../frontend", scriptName: "dev")
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .WithReference(api);
+
+if (builder.Environment.IsDevelopment() && builder.Configuration["DOTNET_LAUNCH_PROFILE"] == "https")
+{
+    // Disable TLS certificate validation in development, see + https://github.com/dotnet/aspire/issues/3324 for more details.
+    frontend.WithEnvironment("NODE_TLS_REJECT_UNAUTHORIZED", "0");
+}
 
 builder.Build().Run();
